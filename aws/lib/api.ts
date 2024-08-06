06394488874e4ca886dc02946816fd34870c3b6f
NiKuma0 <nikuma@Monstr-LPC.>
2024-08-06 17:39:26 +0300

    Added secrets

--- aws/lib/api.ts
+++ aws/lib/api.ts
@@ -1,12 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import * as python from '@aws-cdk/aws-lambda-python-alpha'
+import * as python from '@aws-cdk/aws-lambda-python-alpha';
 import { Construct } from 'constructs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as sm from 'aws-cdk-lib/aws-secretsmanager';
 import * as gateway from 'aws-cdk-lib/aws-apigateway';
 
 export class ApiConstruct extends Construct {
   constructor(scope: Construct, id: string){
     super(scope, id)
+    const secrets = sm.Secret.fromSecretNameV2(this, 'Secrets', 'prod/NoteBot');
     const webhook = new python.PythonFunction( this, "TgWebhook", {
         entry: './src',
         index: 'lambdas/webhook.py',
@@ -16,11 +18,10 @@ export class ApiConstruct extends Construct {
         runtime: Runtime.PYTHON_3_12,
         environment: {
           DEBUG: 'false',
-          BOT_TOKEN: '1717757175:AAGtu3HC_h8dRD7LFEBxjafBODoI_B693i8',
+          BOT_TOKEN: secrets.secretValueFromJson('BOT_TOKEN').toString(),
         },
       }
     )
-
     const authHandler = new python.PythonFunction( this, "AuthHandler", {
         entry: './src/lambdas',  // Now it will not install deps from requirements.txt
         index: 'auth.py',
@@ -29,11 +30,10 @@ export class ApiConstruct extends Construct {
         memorySize: 256,
         runtime: Runtime.PYTHON_3_12,
         environment: {
-          SECRET_TOKEN: 'None',
+          SECRET_TOKEN: secrets.secretValueFromJson('SECRET_TOKEN').toString(),
         },
       }
     )
-
     const authorizer = new gateway.TokenAuthorizer(this, 'TgToken', {
       handler: authHandler,
       identitySource: 'method.request.header.X-Telegram-Bot-Api-Secret-Token',
